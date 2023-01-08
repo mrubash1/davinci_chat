a281d4bc5b5425c27c056d9d6923ed4a061aa533
Matt Rubashkin <mrubash1@gmail.com>
2023-01-08 14:38:59 -0800

    have a base prompt invisible to user

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # davinci_chat
 
-# chatgpt-clone
-
 ### Instructions:
 
 1. Get your OpenAI API key here - https://beta.openai.com/account/api-keys
 2. Create .env file with the following
 ```
 OPENAI_API_KEY= "YOUR OPEN API KEY"
+GRADIO_USERNAME= "EXAMPLE_USERNAME"
+GRADIO_PASSWORD= "EXAMPLE_PASSWORD"
+
 ```
 3. Install the required libraries `pip install -r requirements.txt` 
 4. run `python app.py` 
@@ -16,6 +17,6 @@ OPENAI_API_KEY= "YOUR OPEN API KEY"
 
 ### Sourced information
 - Build Yo'own ChatGPT with OpenAI API &amp; Gradio]
-	- [Code](https://github.com/amrrs/chatgpt-clone/blob/main/README.md)
+	- [Code](https://github.com/amrrs/chatgpt-clone)
 	- [Video](https://www.youtube.com/watch?v=n5nn3mQxrE8&ab_channel=1littlecoder
 
--- app.py
+++ app.py
@@ -17,11 +17,11 @@ GRADIO_PASSWORD = os.getenv("GRADIO_PASSWORD")
 # Input configurations
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
-prompt = "The following is a conversation with an AI story teller. \
+prompt = "The following is a conversation with an AI story teller.\
     The AI is helpful, creative, clever, and very friendly.\
-    \n\nHuman: Hello, who are you? \
-    \nAI: I am an AI that can write imaginative and captivating short stories. \
-    What would you like your story to be about? \
+    \n\nHuman: Hello, who are you?\
+    \nAI: I am an AI that can write imaginative and captivating short stories.\
+    What would you like your story to be about?\
     \nHuman:"
 storyteller_base = "I want you to act as a storyteller.\
     You will come up with entertaining stories that are engaging,\
@@ -34,51 +34,72 @@ storyteller_base = "I want you to act as a storyteller.\
     might engage them better etc. \
     My first request is to tell me the story of\n "
 examples = [
-    ["A venture capitalist who prioritized profit over ethics"],
-    ["Brave dinasour who stood up for his friends in difficult situations"],
+    "A venture capitalist who prioritized profit over ethics",
+    "Brave dinasour who stood up for his friends in difficult situations",
 ]
+
 
 # Functions for OpenAI interactions
 def openai_create(prompt):
     response = openai.Completion.create(
     model="text-davinci-003",
-    # Add the storyteller_ base before the prompt for the human user
-    prompt=storyteller_base+prompt,
+    prompt=prompt,
     temperature=0.9,
-    max_tokens=1200,
+    max_tokens=1400,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0.6,
     stop=[" Human:", " AI:"]
     )
-
     return response.choices[0].text
 
+# Functions for OpenAI interactions
+def openai_create_from_base(prompt, base):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=base+prompt,
+    temperature=0.9,
+    max_tokens=1400,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )
+    return response.choices[0].text
+
+# Gradio requires that we have input and outputs
 def davinci_chat(input, history):
     history = history or []
     s = list(sum(history, ()))
     s.append(input)
     inp = ' '.join(s)
-    output = openai_create(inp)
+    # logic is different if the history exists
+    if not history:
+        output = openai_create_from_base(
+            prompt=inp, 
+            base=storyteller_base)
+    else:
+        output = openai_create(
+            prompt=inp)
     history.append((input, output))
     return history, history
 
-# Gradio app
 block = gr.Blocks()
 with block:
     gr.Markdown("""<h1><center>Generate a story with an AI assistant</center></h1>
     """)
-    chatbot = gr.Chatbot()
     message = gr.Textbox(
-        prompt=prompt)
+        placeholder=prompt)
     examples=examples
+    chatbot = gr.Chatbot()
     state = gr.State()
     submit = gr.Button("SEND")
-    submit.click(davinci_chat, 
+    submit.click(
+        fn=davinci_chat, 
         inputs=[message, state], 
         outputs=[chatbot, state])
 
 block.launch(
     share=False, 
-    #auth=(GRADIO_USERNAME, GRADIO_PASSWORD),
+    auth=(GRADIO_USERNAME, GRADIO_PASSWORD),
     debug=True)
